42845ddf4832aa3197931e497b162e372b72d2fa
Eitor Correa <eitor.candido.correa@gmail.com>
2025-12-02 22:07:20 -0300

    adiciona scirpts `lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>O universo disse eu te amo porque você é amor! ❤️</h1>;
+function Home() {
+  return <h1>O universo disse eu te amo porque você é amor! ❤️</h1>;
 }
 
 export default Home;
